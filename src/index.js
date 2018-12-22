af7f9e870324127ce5e1b59157ac947da653549f
Remy Sharp <remy@remysharp.com>
2018-12-22 15:39:45 +0000

    feat: full coverage of response promises
    Added support for `fetch.seed` - when the value is set, unless it
    matches the current seed, the cache will be wiped. This can be done in
    the code or in the devtools console to make repeat testing quick.
    
    Also removes Proxy requirements, sad, and it was cool, but simpler =
    better.

--- src/index.js
+++ src/index.js
@@ -1,77 +1,61 @@
-import { set, get } from 'idb-keyval';
+import { Store, set, get, clear } from 'idb-keyval';
 
-export default function init() {
+const store = new Store('memfetch', 'memfetch');
+
+export default async function init() {
   const _fetch = window.fetch;
 
   console.warn(
     'Using dev-fetch to override normal fetch behaviour. Make sure this does not land in production'
   );
 
-  if (_fetch.__devfetch) return;
+  if (_fetch.__patched) return;
 
-  const mapping = ['blob', 'formData', 'json', 'text'];
-
-  const createKey = (url, options) =>
-    `dev-fetch:${JSON.stringify({ url, options })}`;
+  const createKey = (url, options) => JSON.stringify({ url, options });
 
   const fetch = async (string, options) => {
     const key = createKey(string, options);
 
-    const cached = await get(key);
+    const cached = await get(key, store);
 
     if (cached) {
+      const { body, status, statusText, headers } = cached;
       return Promise.resolve(
-        new Proxy(cached, {
-          get: function(obj, prop) {
-            if (mapping.includes(prop)) {
-              return () => get(`${key}:result`);
-            }
-            return obj[prop];
-          },
-        })
+        new Response(body, { status, statusText, headers })
       );
     }
 
-    return _fetch(string, options).then(res => {
+    return _fetch(string, options).then(async res => {
       const headers = Array.from(res.headers.entries()).reduce(
         (acc, [key, value]) => ({ ...acc, [key]: value }),
         {}
       );
-      const {
-        ok,
-        redirected,
-        status,
-        statusText,
-        type,
-        url,
-        useFinalURL,
-      } = res;
-      set(key, {
-        headers,
-        ok,
-        redirected,
-        status,
-        statusText,
-        type,
-        url,
-        useFinalURL,
-      });
-      return new Proxy(res, {
-        get: function(obj, prop) {
-          if (mapping.includes(prop)) {
-            return () =>
-              obj[prop]().then(result => {
-                set(`${key}:result`, result);
-                return result;
-              });
-          }
-          return obj[prop];
-        },
-      });
+      const { status, statusText } = res;
+
+      const body = await res.blob();
+      const response = new Response(body, { status, statusText, headers });
+
+      set(key, { headers, status, statusText, body, options }, store);
+
+      return response;
     });
   };
 
-  fetch.__devfetch = true;
+  Object.defineProperty(fetch, 'seed', {
+    set: async newSeed => {
+      const seed = await get('seed', store);
+      if (newSeed != seed) {
+        console.log('clearing cache');
+        await clear(store);
+        await set('seed', newSeed, store);
+        return true;
+      }
+      return false;
+    },
+    get: async () => await get('seed', store),
+  });
+
+  fetch.__patched = true;
 
   window.fetch = fetch;
 }
